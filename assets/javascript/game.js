9d714787354d2ea596c6cb1a917db97dba06ab86
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-19 17:31:56 -0400

    Fixed some layout issues, created Jquery link and updated js to generate random number to DOM

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    //Global variables:
+
+    //Generate random number everytime game starts
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+    $("#randomnumbertext").text("Random Number: " + randomNumber);
+    console.log(randomNumber);
+
+    //Assign a random number to the colour images everytime the game starts
+
+    //Create function to add random numbers from colours into "score"
+
+    //Create function to determine wins/losses
+    //if the score is greater than the random number, loss++
+    //if the score equals random number, wins++
+
+
+
+
+
+
+})
+
+
