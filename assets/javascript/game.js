5d9ebe5359806ee9f3cc917283352a5d7c3aac80
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-21 18:07:58 -0400

    FINAL GAME completed and working upon reset also

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
     //Global variables:
     var ourScore = 0;
-    
-//Generate random number everytime game starts
+
+    //Defining wins and losses variable:
+    var wins = 0;
+    $("#winstext").text("Wins: " + wins);
+    // $("#winstext").text("Wins: " + wins);
+    var loss = 0;
+    $("#losstext").text("Losses: " + loss);
+
+
+    //Generate random number everytime game starts
     var randomNumber = Math.floor(Math.random() * 100) + 1;
     $("#randomnumbertext").text("Random Number: " + randomNumber);
     console.log("Random Number: " + randomNumber);
 
-//Assign a random number to the colour images everytime the game starts:
+    //Assign a random number to the colour images everytime the game starts:
     var blueNumber = Math.floor(Math.random() * 10) + 1;
     console.log("Blue Number: " + blueNumber);
     var yellowNumber = Math.floor(Math.random() * 5) + 1;
@@ -17,26 +25,19 @@ $(document).ready(function () {
     var greenNumber = Math.floor(Math.random() * 100) + 1;
     console.log("Green Number: " + greenNumber);
 
-    //Defining wins and losses variable:
-    var wins = 0;
-    $("#winstext").text("Wins: " + wins);
-    // $("#winstext").text("Wins: " + wins);
-    var loss = 0;
-    $("#losstext").text("Losses: " + loss);
-
     //Reset Game function:
     function reset() {
         ourScore = 0;
-        var randomNumber = Math.floor(Math.random() * 100) + 1;
+        randomNumber = Math.floor(Math.random() * 100) + 1;
         $("#randomnumbertext").text("Random Number: " + randomNumber);
         console.log("Random Number: " + randomNumber);
-        var blueNumber = Math.floor(Math.random() * 10) + 1;
+        blueNumber = Math.floor(Math.random() * 10) + 1;
         console.log("Blue Number: " + blueNumber);
-        var yellowNumber = Math.floor(Math.random() * 5) + 1;
+        yellowNumber = Math.floor(Math.random() * 5) + 1;
         console.log("Yellow Number: " + yellowNumber);
-        var redNumber = Math.floor(Math.random() * 50) + 1;
+        redNumber = Math.floor(Math.random() * 50) + 1;
         console.log("Red Number: " + redNumber);
-        var greenNumber = Math.floor(Math.random() * 100) + 1;
+        greenNumber = Math.floor(Math.random() * 100) + 1;
         console.log("Green Number: " + greenNumber);
         $("#scoretext").text("Your score: " + ourScore);
 
@@ -52,7 +53,7 @@ $(document).ready(function () {
     function lossFunction() {
         alert("You lost!");
         loss++;
-        $("#winstext").text("Losses: " + loss);
+        $("#losstext").text("Losses: " + loss);
         reset();
     }
 
@@ -61,7 +62,7 @@ $(document).ready(function () {
 
     //On click, add the appropriate number to "ourScore":
     $("#blue").on("click", function () {
-        ourScore = parseInt(blueNumber) + parseInt(ourScore);
+        ourScore = blueNumber + ourScore;
         $("#scoretext").text(" Your Score: " + ourScore);
         if (ourScore === randomNumber) {
             winFunction();
@@ -72,7 +73,7 @@ $(document).ready(function () {
         console.log("ourscorevariable: " + ourScore);
     });
     $("#yellow").on("click", function () {
-        ourScore = yellowNumber + parseInt(ourScore);
+        ourScore = yellowNumber + ourScore;
         $("#scoretext").text(" Your Score: " + ourScore);
         if (ourScore === randomNumber) {
             winFunction();
@@ -83,7 +84,7 @@ $(document).ready(function () {
         console.log("ourscorevariable: " + ourScore);
     });
     $("#red").on("click", function () {
-        ourScore = parseInt(redNumber) + parseInt(ourScore);
+        ourScore = redNumber + ourScore;
         $("#scoretext").text(" Your Score: " + ourScore);
         if (ourScore === randomNumber) {
             winFunction();
@@ -94,7 +95,7 @@ $(document).ready(function () {
         console.log("ourscorevariable: " + ourScore);
     });
     $("#green").on("click", function () {
-        ourScore = parseInt(greenNumber) + parseInt(ourScore);
+        ourScore = greenNumber + ourScore;
         $("#scoretext").text(" Your Score: " + ourScore);
         if (ourScore === randomNumber) {
             winFunction();
@@ -105,6 +106,6 @@ $(document).ready(function () {
         console.log("ourscorevariable: " + ourScore);
     });
 
-})
+});
 
 
